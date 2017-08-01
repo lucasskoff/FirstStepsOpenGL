62bcc8cf743b45f841f3b8c87451314e46692025
lskoff <lucasskoff@gmail.com>
2017-07-31 20:03:39 -0500

    My first triangle!

--- FirstStepsOpenGL/Application.cpp
+++ FirstStepsOpenGL/Application.cpp
@@ -5,8 +5,26 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+//Simple vertex shader program
+const char *vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+//Simple fragment shader program
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
 int main()
 {
+	//GLFW
+	//----------------------------------------------------------------------------
 	//Initialize and configure glfw
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -24,28 +42,123 @@ int main()
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//Load all OpenGL function pointers
+	//GLAD
+	//----------------------------------------------------------------------------
+	//Load all OpenGL function pointers with GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	
-	//render loop
+
+	//Shader
+	//---------------------------------------------------------------------------
+	//Create a vertex shader object and give it a unique id.
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	//Compile our program above and bind it to an object with our id.
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Test our shader compilation
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Create a fragment shader object and give it a unique id.
+	 unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//Compile our shader program and bind it an object with our id.
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//Test our fragement shader.
+	if (!success) {
+		glGetProgramInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Create an overall shader program and assign it a unique id.
+	unsigned int shaderProgram = glCreateProgram();
+
+	//Bind both our vertex shader and fragment shader to our new shader program.
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	//Link our overall shader program to the larger graphics program.
+	glLinkProgram(shaderProgram);
+
+	//Test our overall shader program.
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Delete vertext & fragment, as they are now linked so we no longer need them.
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//Vertex Data / Vertext Attributes
+	//---------------------------------------------------------------------------
+	//An array of points for our triangle. Only valid points are between -1 and 1.
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	//Create a vertex buffer and vertext array. Give it a unique id.
+	unsigned int VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	//Have to bind vertex array first.
+	glBindVertexArray(VAO);
+
+	//Bind our vertext buffer into a GL_ARRAY. Static is fine as our triangle will not be moving.
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Tell OpenGL how to interpret vertex data when rendering.
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//Uncomment to display vertices in wireframe
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	//Render Loop
+	//---------------------------------------------------------------------------
 	while (!glfwWindowShouldClose(window))
 	{
+		// input
+		processInput(window);
+
+		//render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// input
-		processInput(window);
+		// draw our triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindVertexArray(0); //No need to unbind each time as we only draw one triangle
 
 		//swap buffers and obtain all IO events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-
+	//Clean Up
+	//---------------------------------------------------------------------------
 	//glfw terminate to clear all allocated glfw resources.
 	glfwTerminate();
 	return 0;
