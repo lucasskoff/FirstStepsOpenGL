2c4b8a16703370541897f2991dfc8cd57a011c3e
lskoff <lucasskoff@gmail.com>
2017-08-24 21:32:08 -0500

    Explored how to create rectangle with two triangles

--- FirstStepsOpenGL/Application.cpp
+++ FirstStepsOpenGL/Application.cpp
@@ -101,22 +101,29 @@ int main()
 	//Vertex Data / Vertex Attributes
 	//---------------------------------------------------------------------------
 	float vertices[] = {
-		// first triangle
-		-0.9f, -0.5f, 0.0f,  // left 
-		-0.0f, -0.5f, 0.0f,  // right
-		-0.45f, 0.5f, 0.0f,  // top 
+		// first square
+		 0.0f,  1.0f, 0.0f,  // top right
+		 0.0f,  0.0f, 0.0f,  // bottom right
+		-1.0f,  1.0f, 0.0f,  // top left
+		-1.0f,  0.0f, 0.0f   // bottom left
 	};
 
-	// second triangle
-	float vertices2[] = {
+	// second square
+	/* vertices2[] = {
 		0.0f, -0.5f, 0.0f,  // left
 		0.9f, -0.5f, 0.0f,  // right
 		0.45f, 0.5f, 0.0f   // top 
-	};
+	};*/
 
-	unsigned int VBOs[2], VAOs[2];
-	glGenVertexArrays(2, VAOs);
-	glGenBuffers(2, VBOs);
+	unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+	};  
+
+	unsigned int VBOs[1], VAOs[1], EBO;
+	glGenVertexArrays(1, VAOs);
+	glGenBuffers(1, VBOs);
+	glGenBuffers(1, &EBO);
 
 	//Set up First Triangle
 	glBindVertexArray(VAOs[0]);
@@ -124,13 +131,15 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
 	//Set up second Triangle
-	glBindVertexArray(VAOs[1]);
+	/*glBindVertexArray(VAOs[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(0);*/
 
 	/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -161,9 +170,9 @@ int main()
 		//ourShader.use();
 		glUseProgram(shaderProgramOrange);
 		glBindVertexArray(VAOs[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glUseProgram(shaderProgramYellow);
-		glBindVertexArray(VAOs[1]);
+		//glBindVertexArray(VAOs[1]);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//glfw: swap buffers and obtain all IO events
