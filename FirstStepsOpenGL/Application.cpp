7435f081b6b276100f0f27f2a8fcc13cb5cbb599
lskoff <lucasskoff@gmail.com>
2017-08-26 17:45:55 -0500

    OWN - Drew two squares, different colors.

--- FirstStepsOpenGL/Application.cpp
+++ FirstStepsOpenGL/Application.cpp
@@ -103,37 +103,48 @@ int main()
 	float vertices[] = {
 		// first square
 		 0.0f,  1.0f, 0.0f,  // top right
-		 0.0f,  0.0f, 0.0f,  // bottom right
-		-1.0f,  1.0f, 0.0f,  // top left
-		-1.0f,  0.0f, 0.0f   // bottom left
+		 -1.0f,  1.0f, 0.0f,  // top left
+		 -1.0f,  0.0f, 0.0f,   // bottom left
+		 0.0f,  0.0f, 0.0f  // bottom right
 	};
 
 	// second square
-	/* vertices2[] = {
-		0.0f, -0.5f, 0.0f,  // left
-		0.9f, -0.5f, 0.0f,  // right
-		0.45f, 0.5f, 0.0f   // top 
-	};*/
+	float vertices2[] = {
+		1.0f,  1.0f, 0.0f,  // top right
+		0.0f,  1.0f, 0.0f,  // top left
+		0.0f,  0.0f, 0.0f,   // bottom left
+		1.0f,  0.0f, 0.0f  // bottom right
+		
+	};
 
 	unsigned int indices[] = {  // note that we start from 0!
     0, 1, 3,   // first triangle
     1, 2, 3    // second triangle
 	};  
 
-	unsigned int VBOs[1], VAOs[1], EBO;
-	glGenVertexArrays(1, VAOs);
-	glGenBuffers(1, VBOs);
-	glGenBuffers(1, &EBO);
+	unsigned int VBOs[2], VAOs[2], EBOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
+	glGenBuffers(2, EBOs);
 
-	//Set up First Triangle
+	//Set up First Square
 	glBindVertexArray(VAOs[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
+	//Set up second square
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//Set up second Triangle
 	/*glBindVertexArray(VAOs[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
@@ -170,10 +181,12 @@ int main()
 		//ourShader.use();
 		glUseProgram(shaderProgramOrange);
 		glBindVertexArray(VAOs[0]);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glUseProgram(shaderProgramYellow);
-		//glBindVertexArray(VAOs[1]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAOs[1]);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		//glfw: swap buffers and obtain all IO events
 		glfwSwapBuffers(window);
