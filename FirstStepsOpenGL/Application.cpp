477a2ab874d5e508da4fba0ab23a6a89dfcb08ca
lskoff <lucasskoff@gmail.com>
2017-09-12 13:20:20 -0500

    All even numbered cubes rotate clockwise, all odd numbered rotate counter-clockwise.

--- FirstStepsOpenGL/Application.cpp
+++ FirstStepsOpenGL/Application.cpp
@@ -222,13 +222,13 @@ int main()
 		for (unsigned int i = 0; i < 10; i++) {
 			glm::mat4 model;
 			model = glm::translate(model, cubePositions[i]);
-			if (i % 3 == 0) {
-				float angle = 20.0f * i;
-				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			if(i % 2 == 0)
+			{
+				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 			}
 			else
 			{
-				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+				model = glm::rotate(model, (-1)*(float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 			}
 			ourShader.setMat4("model", model);
 
